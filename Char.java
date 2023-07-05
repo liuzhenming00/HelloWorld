7d2084fc54df14c8cd91490fd31a63fffa2f022a
DESKTOP-DA8L7GC\刘 <liuzhenming00@gmail.com>
2023-07-05 22:53:12 +0800

    no message

--- Char.java
+++
@@ -1,6 +0,0 @@
-public class Char{
-	public static void main(String[] args) {
-		char exprctedGrade='B';
-		System.out.println(exprctedGrade);
-	}
-}
--- Final.java
+++
@@ -1,9 +0,0 @@
-public class Final {
-	public static void main(String[] args) { 
-    final double pi = 3.14;
-    
-    System.out.println(pi);
-
-
-	}
-}
